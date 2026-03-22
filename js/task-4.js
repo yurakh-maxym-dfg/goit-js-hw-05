4e10b89c71f3cef9b3a4931f8d60c5ae79c0a680
yurakh-maxym-dfg <maksaaasssdfg@gmail.com>
2026-03-22 19:56:43 +0200

    task-4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,62 @@
 'use strict';
+
+const getTotalBalanceByGender = (users, gender) => {
+    if (gender === 'male') {
+        const totalMale = users
+            .filter(user => user.gender === 'male')
+            .reduce((total, user) => {
+                return total + user.balance;
+            }, 0);
+            return totalMale;
+        }
+    else {
+        const totalFemale = users
+            .filter(user => user.gender === 'female')
+            .reduce((total, user) => {
+                return total + user.balance;
+            }, 0);
+            return totalFemale;
+    };
+};
+
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
+
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
